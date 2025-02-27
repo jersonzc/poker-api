4b28c8ef9ab944474d34d3e6b5ac4d18809f05a1
Jerson Zúñiga <jzunigacoayla@gmail.com>
2025-02-26 21:55:04 -0500

    feat: create in memory store

--- main.go
+++ main.go
@@ -6,7 +6,13 @@ import (
 	"poker-api/poker"
 )
 
+type InMemoryPlayerStore struct{}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
 func main() {
-	server := &poker.PlayerServer{}
+	server := &poker.PlayerServer{&InMemoryPlayerStore{}}
 	log.Fatal(http.ListenAndServe(":8080", server))
 }
