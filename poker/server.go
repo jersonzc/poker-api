9e8f407805324e914581dcfe4cff727de515b1ba
Jerson Zuniga <jzunigacoayla@gmail.com>
2025-04-23 10:22:15 -0500

    feat: store winner name

--- poker/server.go
+++ poker/server.go
@@ -16,16 +16,17 @@ type PlayerServer struct {
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
 	switch r.Method {
 	case http.MethodGet:
-		p.showScore(w, r)
+		p.showScore(w, player)
 	case http.MethodPost:
-		p.processWin(w)
+		p.processWin(w, player)
 	}
 }
 
-func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
+func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
 	score := p.Store.GetPlayerScore(player)
 
 	if score == 0 {
@@ -35,7 +36,7 @@ func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, score)
 }
 
-func (p *PlayerServer) processWin(w http.ResponseWriter) {
-	p.Store.RecordWin("Pepper")
+func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
+	p.Store.RecordWin(player)
 	w.WriteHeader(http.StatusAccepted)
 }
