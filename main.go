35ba41de2e459f4296d6c76ee0a5121897e728bf
Jerson Zuniga <jzunigacoayla@gmail.com>
2025-04-23 10:44:18 -0500

    feat: use in-memory store

--- main.go
+++ main.go
@@ -6,15 +6,8 @@ import (
 	"poker-api/poker"
 )
 
-type InMemoryPlayerStore struct{}
-
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return 123
-}
-
-func (i *InMemoryPlayerStore) RecordWin(name string) {}
-
 func main() {
-	server := &poker.PlayerServer{&InMemoryPlayerStore{}}
+	store := NewInMemoryPlayerStore()
+	server := &poker.PlayerServer{store}
 	log.Fatal(http.ListenAndServe(":8080", server))
 }
